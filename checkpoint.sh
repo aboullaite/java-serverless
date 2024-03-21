5ec6a1d1b859a4f885c33a38d5bf5e23aa4471b8
Mohammed Aboullaite <maboullaite@spotify.me>
2024-03-21 22:52:44 +0100

    fix crac build issue
    Signed-off-by: Mohammed Aboullaite <maboullaite@spotify.me>

--- checkpoint.sh
+++ checkpoint.sh
@@ -7,9 +7,9 @@ case $(uname -m) in
 esac
 
 echo "Using CRaC enabled JDK $url"
-docker build --platform linux/amd64 -t chatbot-crac:0.0.1 -f Dockerfile.CRAC --build-arg CRAC_JDK_URL=$url .
-docker run -d --privileged --rm --name=chatbot-crac --ulimit nofile=1024 -p 8080:8080 -v $(pwd)/target:/opt/mnt -e FLAG=$1 chatbot-crac:0.0.1
+docker build -t chatbot-crac:builder -f Dockerfile.CRAC --build-arg CRAC_JDK_URL=$url .
+docker run -d --privileged --rm --name=chatbot-crac --ulimit nofile=1024 -p 8080:8080 -v $(pwd)/target:/opt/mnt -e FLAG=$1 chatbot-crac:builder
 echo "Please wait during checkpoint creation..."
 sleep 10
-docker commit --change='ENTRYPOINT ["/opt/app/entrypoint.sh"]' $(docker ps -qf "name=chatbot-crac") chatbot-crac:checkpoint
+docker commit --change='ENTRYPOINT ["/opt/app/entrypoint.sh"]' $(docker ps -qf "name=chatbot-crac") chatbot-crac:0.0.1
 docker kill $(docker ps -qf "name=chatbot-crac")
